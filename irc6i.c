99950cc7eccd49d527e601454009dd9ffd700c2a
yentup <yentup@users.noreply.github.com>
2016-04-12 15:52:44 -0400

    Initial working commit (includes gitignore)
    Currently separate files for ipv4 and ipv6; working on irc6i.c

---
+++ irc6i.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+void error(char *msg) {
+	perror(msg);
+	exit(1);
+}
+
+void sockwrite(int sockfd, char msg[512]) {
+	int n = write(sockfd, msg, strlen(msg));
+	if (n < 0)
+		error("Error writing to socket");
+}
+
+int main(int argc, char **argv) {
+	int sockfd, portno, n;
+	struct sockaddr_in6 serv_addr;
+	struct hostent *server;
+	char nick_msg[512], user_msg[512];
+
+	if (argc < 5) {
+		fprintf(stderr, "Usage: %s server:port nickname username realname\n", argv[0]);
+		return 0;
+	}
+
+	// Split server:port
+	char **serv_port = (char **) calloc(32, sizeof(char *));
+	char *token;
+	int i = 0;
+	while ( (token = strsep(&argv[1], ":")) ) {
+		serv_port[i++] = token;
+	}
+
+	if (i != 2) {
+		fprintf(stderr, "Incorrect server and port.\n\
+Usage: %s server:port nickname username realname\n", argv[0]);
+		return 0;
+	}
+	portno = atoi(serv_port[1]);
+
+	// Assign messages to send to server
+	char *nick = argv[2], *user = argv[3], *real = argv[4];
+	sprintf(nick_msg, "NICK %s\r\n", nick);
+	sprintf(user_msg, "USER %s 0 * :%s\r\n", user, real);
+
+	// Create the socket
+	sockfd = socket(AF_INET6, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+		error("Error opening socket");
+	}
+
+	// Set the fields in serv_addr
+	server = gethostbyname2(serv_port[0], AF_INET6);
+	if (server == NULL) {
+		fprintf(stderr, "Error: no such host\n");
+		return 0;
+	}
+
+	memset((char *) &serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin6_flowinfo = 0;
+	serv_addr.sin6_family = AF_INET6;
+	memmove(
+		(char *) &serv_addr.sin6_addr.s6_addr,
+		(char *) server->h_addr,
+		server->h_length
+	);
+	serv_addr.sin6_port = htons(portno);
+
+	// Connect the socket
+	if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+		error("Error connecting");
+
+	// Communicate with the server
+	sockwrite(sockfd, nick_msg);
+	sockwrite(sockfd, user_msg);
+
+	char buffer[512];
+	while ( (n = read(sockfd, buffer, 511)) > 0 ) {
+		printf("%s", buffer);
+		memset(buffer, 0, 512);
+	}
+	if (n < 0) {
+		error("Error reading from socket");
+	}
+
+	// Close the socket
+	close(sockfd);
+
+	return 0;
+}
