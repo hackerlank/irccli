0cb1d20b49948a8a022b8744496c6926ebe503b0
Jack Cogdill <yentup@users.noreply.github.com>
2016-04-18 00:48:22 -0400

    Removed debug messages and comments

--- src/sock_util.c
+++ src/sock_util.c
@@ -20,8 +20,6 @@ void connect_socket(int *sockfd, char *server_name, int portno) {
 	if (server == NULL)
 		ipv6_success = 0;
 
-	printf("Using IPv%s\n", (ipv6_success) ? "6" : "4");///////
-
 	// IPv6
 	if (ipv6_success) {
 		struct sockaddr_in6 serv_addr;
@@ -36,26 +34,14 @@ void connect_socket(int *sockfd, char *server_name, int portno) {
 		);
 		serv_addr.sin6_port = htons(portno);
 
-		printf("Connecting...");///////
-
 		// Connect the socket
 		if (connect(*sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-			// error("Error connecting");
 			ipv6_success = 0;
 		}
-
-		///////
-		if (ipv6_success == 1)
-			printf("connected!\n");
-		else
-			printf("failed.\n");
-		///////
 	}
 
 	// IPv6 failed, try using IPv4
 	if (!ipv6_success) {
-		printf("Using IPv4\n");///////
-
 		*sockfd = socket(AF_INET, SOCK_STREAM, 0);
 		if (*sockfd < 0) {
 			error("Error opening socket");
@@ -66,8 +52,6 @@ void connect_socket(int *sockfd, char *server_name, int portno) {
 			fprintf(stderr, "Error: no such host\n");
 			exit(0);
 		}
-
-		printf("%s\n", server->h_name); ///////////
 
 		struct sockaddr_in serv_addr;
 		// Set the fields in serv_addr
